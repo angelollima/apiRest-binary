ff45e1f4f197f7721f64fb37cfb828fadd74d71c
Angelollima <angeloantonio442@gmail.com>
2022-08-11 00:16:24 -0300

    new keys/values

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
-from tokenize import Number
 import uvicorn
 from fastapi import FastAPI
 import string
+import json
 
 app = FastAPI()
 
 #soma de binarios
 
-*teste, = string.ascii_lowercase
-*teste1, = string.digits
-*lista, = "".join(c + c.upper() for c in teste)
+*digits, = string.digits
+*punctuation_methods, = string.punctuation
+*letters_lowercase, = string.ascii_lowercase
+*upper_lower_letters, = "".join(c + c.upper() for c in letters_lowercase)
+*words_accent, = "".join(c + c.upper() for c in """
+áâãàäåæāăąªçÇcĆčČďđĎĐéÉêÊèÈëËēĒėĖęĘěĚĕĔəƏ
+ģĢğĞíÍìÌîÎïÏīĪįĮıİķĶĺĹļĻľĽłŁñÑńŃņŅňŇóÓôÔõÕòÒöÖøØōŌőŐœŒ
+ŕŔřŘß§śŚšŠşŞþÞťŤțȚţŢúÚüÜùÙûÛūŪůŮűŰųŲũŨýÝźŹżŻžŽ
+""")
 
 @app.get("/")
 def everything(letter: str | None=None, number: int | None=None):
-    all_letters = {letter: f'{ord(letter):08b}' for letter in lista}
-    all_numbers = {number: f'{ord(number):08b}' for number in teste1}
+    all_letters = {letter: f'{ord(letter):08b}' for letter in upper_lower_letters}
+    accentuation_letters = {accentuation: f'{ord(accentuation):08b}' for accentuation in words_accent}
+    all_numbers = {number: f'{ord(number):08b}' for number in digits}
+    all_punctuation = {punctuation: f'{ord(punctuation):08b}' for punctuation in punctuation_methods}
     if letter is None and number is None:
-        """thing = {"letters": all_letters}
-        other_thing = {"numbers": all_numbers}
-        my_dict = {"thing": thing, "other_thing": other_thing}
-        return my_dict"""
-        return {"letters": all_letters, "numbers": all_numbers}
+        resposta = ({
+            "letters": all_letters,
+            "letters with accent": [accentuation_letters],
+            "numbers": all_numbers,
+            "punctuations": all_punctuation
+        })
+        return resposta
+        #return {"letters": all_letters, "numbers": all_numbers}
     else:
         response = []
         for k, v in all_letters.items():
             if k.lower() == letter:
                 response.append({k: v})
+        for k, v in accentuation_letters.items():
+            if k == letter:
+                response.append({k, v})
         for k, v in all_numbers.items():
             if int(k) == number:
                 response.append({k: v})
@@ -40,7 +54,7 @@ def letters():
 
 @app.get("/numbers/")
 def numbers():
-    all_numbers = {"numbers":{number: f'{ord(number):08b}' for number in teste1}}
+    all_numbers = {"numbers":{number: f'{ord(number):08b}' for number in digits}}
     return all_numbers"""
 
 if __name__ == "__main__":
