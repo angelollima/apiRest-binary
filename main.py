7902e8ab00c055b9432f5dcca8a65a7becd2b48e
Angelollima <angeloantonio442@gmail.com>
2022-08-11 17:20:42 -0300

    new numbers

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 import uvicorn
 from fastapi import FastAPI
 import string
-import json
 
 app = FastAPI()
 
-#soma de binarios
-
-*digits, = string.digits
+digits = [i for i in range(5000)]
+digits1 = []
+for i in digits:
+    digits1.append(str(i))
 *punctuation_methods, = string.punctuation
 *letters_lowercase, = string.ascii_lowercase
 *upper_lower_letters, = "".join(c + c.upper() for c in letters_lowercase)
-*words_accent, = "".join(c + c.upper() for c in """
-áâãàäåæāăąªçÇcĆčČďđĎĐéÉêÊèÈëËēĒėĖęĘěĚĕĔəƏ
-ģĢğĞíÍìÌîÎïÏīĪįĮıİķĶĺĹļĻľĽłŁñÑńŃņŅňŇóÓôÔõÕòÒöÖøØōŌőŐœŒ
-ŕŔřŘß§śŚšŠşŞþÞťŤțȚţŢúÚüÜùÙûÛūŪůŮűŰųŲũŨýÝźŹżŻžŽ
-""")
+*words_accent, = "".join(c + c.upper() for c in """áâãàäåæāăąªçÇcĆčČďđĎĐéÉêÊèÈëËēĒėĖęĘěĚĕĔəƏģĢğĞíÍìÌîÎïÏīĪįĮıİķĶĺĹļĻľĽłŁñÑńŃņŅňŇóÓôÔõÕòÒöÖøØōŌőŐœŒŕŔřŘß§śŚšŠşŞþÞťŤțȚţŢúÚüÜùÙûÛūŪůŮűŰųŲũŨýÝźŹżŻžŽ""")
 
 @app.get("/")
 def everything(letter: str | None=None, number: int | None=None):
     all_letters = {letter: f'{ord(letter):08b}' for letter in upper_lower_letters}
-    accentuation_letters = {accentuation: f'{ord(accentuation):08b}' for accentuation in words_accent}
-    all_numbers = {number: f'{ord(number):08b}' for number in digits}
+    all_accentuation_letters = {accentuation: f'{ord(accentuation):08b}' for accentuation in words_accent}
+    all_numbers = {number: f"{number:08b}" for number in digits}
     all_punctuation = {punctuation: f'{ord(punctuation):08b}' for punctuation in punctuation_methods}
     if letter is None and number is None:
         resposta = ({
             "letters": all_letters,
-            "letters with accent": [accentuation_letters],
-            "numbers": all_numbers,
-            "punctuations": all_punctuation
+            "letters with accent": [all_accentuation_letters],
+            "punctuations": all_punctuation,
+            "numbers": all_numbers
         })
         return resposta
         #return {"letters": all_letters, "numbers": all_numbers}
@@ -37,7 +33,7 @@ def everything(letter: str | None=None, number: int | None=None):
         for k, v in all_letters.items():
             if k.lower() == letter:
                 response.append({k: v})
-        for k, v in accentuation_letters.items():
+        for k, v in all_accentuation_letters.items():
             if k == letter:
                 response.append({k, v})
         for k, v in all_numbers.items():
@@ -47,15 +43,21 @@ def everything(letter: str | None=None, number: int | None=None):
 
 
 
-"""@app.get("/letters/")
+@app.get("/letters/")
 def letters():
-    all_letters = {"letters":{letter: f'{ord(letter):08b}' for letter in lista}}
-    return all_letters
+    all_letters = {letter: f'{ord(letter):08b}' for letter in upper_lower_letters}
+    all_accentuation_letters = {accentuation: f'{ord(accentuation):08b}' for accentuation in words_accent}
+    return all_letters, all_accentuation_letters
 
 @app.get("/numbers/")
 def numbers():
-    all_numbers = {"numbers":{number: f'{ord(number):08b}' for number in digits}}
-    return all_numbers"""
+    all_numbers = {number: f"{number:08b}" for number in digits}
+    return all_numbers
+
+@app.get("/numbers/")
+def punctuation():
+    all_punctuation = {punctuation: f'{ord(punctuation):08b}' for punctuation in punctuation_methods}
+    return all_punctuation
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
